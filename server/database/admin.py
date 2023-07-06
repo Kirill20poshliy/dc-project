51637f183489049467b6ede4893e63b96d4f859e
a.krutyakova <alexandrakru17@gmail.com>
2023-07-06 14:15:38 +0300

    тест

--- server/database/admin.py
+++ server/database/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile, Message, Attachment
-
-
+"""
+test
+"""
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'first_name', 'last_name', 'middle_name', 'login', 'type')
