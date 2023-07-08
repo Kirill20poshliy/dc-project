2cb26b779ddadd5ff181a908d6806103ba92ac66
Ruslan Shaydullin <shaydullin0@gmail.com>
2023-07-09 00:21:33 +0300

    Регистрация по JWT

--- server/api/urls.py
+++ server/api/urls.py
@@ -15,6 +15,12 @@ from drf_spectacular.views import (
     SpectacularSwaggerView
 )
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 app_name = 'apiapp'
 
 
@@ -27,4 +33,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='apiapp:schema'), name='swagger-ui'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
