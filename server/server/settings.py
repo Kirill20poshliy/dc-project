2b1f88ffe98138782ea62eb3dfa6f687b346cec0
Kirichek Alexey <rokirokz@mail.ru>
2023-07-09 14:47:21 +0300

    JWT

--- server/server/settings.py
+++ server/server/settings.py
@@ -139,9 +139,13 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
 }
 
 
